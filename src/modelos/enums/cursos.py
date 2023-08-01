ad92940702086d55b94057d48b52b40c2f22a07b
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2023-08-01 18:51:08 -0300

    Adicionada lista de disciplinas

--- src/modelos/enums/cursos.py
+++ src/modelos/enums/cursos.py
@@ -1,3 +1,63 @@
+# Disciplinas separadas por período
 disciplinas = [
-    ''
+    # Primeiro
+    'Cálculo 1',
+    'Introdução à programação',
+    'Lógica matemática',
+    'Geometria analítica',
+    'Introdução à computação',
+
+    # Segundo
+    'POO',
+    'Cálculo 2',
+    'Álgebra linear',
+    'Física'
+    'AED 1',
+
+    # Terceiro
+    'Probabilidade e estatística',
+    'Sistemas digitais',
+    'AED 2',
+    'Matemática discreta',
+    'Metodologia científica',
+    'Inglês',
+
+    # Quarto
+    'Arquitetura de computadores',
+    'Engenharia de software',
+    'Paradigmas de linguagens de programação',
+    'Banco de dados',
+    'Projeto e análise de algoritmos',
+
+    # Quinto
+    'Teoria da computação',
+    'Sistemas de informação',
+    'Redes de computadores',
+    'Sistemas operacionais',
+    'Inteligência artificial',
+
+    # Sexto
+    'Empreendedorismo',
+    'Compiladores',
+    'Reconhecimento de padrões',
+    'Sistemas distribuídos',
+    'Computação gráfica',
+
+    # Sétimo
+    'Interação humano-computador'
+    'Projeto de desenvolvimento',
+    'Computadores e sociedade',
+    'Optativa 1',
+    'Optativa 2',
+
+    # Oitavo
+    'Optativa 3',
+    'Optativa 4',
+    'Optativa 5',
+    'Optativa 6',
+    'Optativa 7',
+
+    # Nono
+    'Estágio supervisionado obrigatório',
+    'TCC'
 ]
