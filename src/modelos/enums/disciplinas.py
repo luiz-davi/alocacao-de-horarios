5ce558e671738ecb540c87640263ef40a889c3eb
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2023-08-01 19:39:31 -0300

    Padronizadas informações de disciplina

---
+++ src/modelos/enums/disciplinas.py
@@ -0,0 +1,63 @@
+# Disciplinas separadas por período
+disciplinas = [
+    # Primeiro
+    'calculo 1',
+    'introducao a programacao',
+    'logica matematica',
+    'geometria analitica',
+    'introducao a computacao',
+
+    # Segundo
+    'poo',
+    'calculo 2',
+    'algebra linear',
+    'fisica'
+    'aed 1',
+
+    # Terceiro
+    'probabilidade e estatistica',
+    'sistemas digitais',
+    'aed 2',
+    'matematica discreta',
+    'metodologia cientifica',
+    'ingles',
+
+    # Quarto
+    'arquitetura de computadores',
+    'engenharia de software',
+    'paradigmas de linguagens de programacao',
+    'banco de dados',
+    'projeto e analise de algoritmos',
+
+    # Quinto
+    'teoria da computacao',
+    'sistemas de informacao',
+    'redes de computadores',
+    'sistemas operacionais',
+    'inteligencia artificial',
+
+    # Sexto
+    'empreendedorismo',
+    'compiladores',
+    'reconhecimento de padroes',
+    'sistemas distribuidos',
+    'computacao grafica',
+
+    # Sétimo
+    'interacao humano-computador'
+    'projeto de desenvolvimento',
+    'computadores e sociedade',
+    'optativa 1',
+    'optativa 2',
+
+    # Oitavo
+    'optativa 3',
+    'optativa 4',
+    'optativa 5',
+    'optativa 6',
+    'optativa 7',
+
+    # Nono
+    'estagio supervisionado obrigatorio',
+    'tcc'
+]
