5e1e78b16657bf647b3a7917036218c3000863b9
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2023-08-03 00:32:25 -0300

    Adicionado período na disciplina

--- src/modelos/disciplina.py
+++ src/modelos/disciplina.py
@@ -1,6 +1,7 @@
 class Disciplina:
 
-    def __init__(self, nome, quantidade_de_aulas, docentes=None):
+    def __init__(self, nome, quantidade_de_aulas, periodo, docentes=None):
         self.nome = nome
         self.quantidade_de_aulas = quantidade_de_aulas
+        self.periodo = periodo
         self.docentes = docentes if docentes is not None else []
